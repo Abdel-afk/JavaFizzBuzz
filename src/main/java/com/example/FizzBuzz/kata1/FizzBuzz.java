3958ac06a7032e653b6ae8355dc3c5357218e382
Abdelhakim <abdelhakim8304@gmail.com>
2023-03-30 13:49:38 +0200

    first commit

---
+++ src/main/java/com/example/FizzBuzz/kata1/FizzBuzz.java
@@ -0,0 +1,8 @@
+package com.example.FizzBuzz.kata1;
+
+public class FizzBuzz {
+
+    public String execute(Integer num){
+        return "hola";
+    }
+}
---
+++ src/test/java/com/example/FizzBuzz/kata1/FizzBuzzTest.java
@@ -0,0 +1,17 @@
+package com.example.FizzBuzz.kata1;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class FizzBuzzTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @Test
+    void execute() {
+    }
+}
